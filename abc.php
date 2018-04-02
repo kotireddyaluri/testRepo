6a2c2c56e841a75ec21239f552718cd156e087e1
Koti Reddy Aluri <kotireddyaluri@users.noreply.github.com>
2018-04-02 10:35:27 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -5,5 +5,5 @@
 $output = shell_exec('ls');
 echo "<pre>$output</pre>";
 
-system('/usr/local/bin/score a0ca6189-282c-4968-8193-a27f5cf79176');
+//system('/usr/local/bin/score a0ca6189-282c-4968-8193-a27f5cf79176');
 ?>
