8a388058fd6be69c0602677a4c69c4f69b50e4d0
Koti Reddy Aluri <kotireddyaluri@users.noreply.github.com>
2018-04-02 10:19:18 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -1,1 +1,7 @@
-/usr/local/bin/score a0ca6189-282c-4968-8193-a27f5cf79176
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+///usr/local/bin/score a0ca6189-282c-4968-8193-a27f5cf79176
+
+?>
